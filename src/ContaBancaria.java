f6c212d8309814b7af17cbb79fd9e3a49d2f1c8f
getulio.souza <getulio.souza@evolutix.com.br>
2024-08-13 19:19:24 -0300

    bugfix: deposito-e-saque-funcionando

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,22 +1,24 @@
+import java.math.BigDecimal;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaBancaria {
+  @SuppressWarnings("deprecation")
   public static void main(String[] args) {
-    System.out.println("hello world");
 
-    //Conhecer e importar a classe scanner
+    // Conhecer e importar a classe scanner
     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-    //exibir as mensagem para o nosso usuario
+    // exibir as mensagem para o nosso usuario
     System.out.println("Digite seu nome");
-    //obter pela classe scanner os valores digitados no terminal
     String nome = scanner.next();
 
-    //Obtendo a agencia com 4 digitos
+    System.out.println("Bem-vindo(a) " + nome);
+
+    // Obtendo a agencia com 4 digitos
     String agencia;
     while (true) {
-      System.out.println("Digite sua agencia");
+      System.out.println("Digite sua agencia (formato XXXX)");
       agencia = scanner.next();
 
       if (agencia.matches("\\d{4}")) {
@@ -36,10 +38,42 @@ public class ContaBancaria {
       System.out.println("Conta invalida. Deve seguir o formato XXXXX-XX");
     }
 
-    System.out.println("Bem-vindo(a): " + nome);
-    System.out.println("Sua agência é: " + agencia);
-    System.out.println("Sua conta é: " + conta);
-    System.out.println("O que você deseja fazer hoje? Selecione uma das opcoes abaixo:");
-    //obtendo o numero da conta
+    BigDecimal saldoAtual = new BigDecimal("4300.00");
+
+    // escolhendo entre deposito ou saque
+    System.out.println("Escolha uma das opcoes a seguir:");
+    System.out.println("1. Extrato");
+    System.out.println("2. Deposito");
+    System.out.println("3. Saque");
+    int opcao = scanner.nextInt();
+
+    switch (opcao) {
+
+      case 1:
+        System.out.println("seu saldo atual eh de: " + saldoAtual.setScale(2, BigDecimal.ROUND_HALF_EVEN));
+        break;
+
+      case 2:
+        // realizando um deposito na conta
+        System.out.println("Digite um valor para fazer seu deposito:");
+        BigDecimal deposito = scanner.nextBigDecimal();
+        saldoAtual = saldoAtual.add(deposito);
+        System.out.println("Deposito realizado no valor de: " + deposito.setScale(2, BigDecimal.ROUND_HALF_EVEN));
+        System.out.println("Valor atual da conta é de: " + saldoAtual.setScale(2, BigDecimal.ROUND_HALF_EVEN));
+        break;
+
+      case 3:
+        // realizando um saque na conta
+        System.out.println("Digite um valor para fazer seu saque:");
+        BigDecimal saque = scanner.nextBigDecimal();
+        saldoAtual = saldoAtual.subtract(saque);
+        System.out.println("Saque realizado no valor de: " + saque.setScale(2, BigDecimal.ROUND_HALF_EVEN));
+        System.out.println("Valor atual da conta é de: " + saldoAtual.setScale(2, BigDecimal.ROUND_HALF_EVEN));
+        break;
+        
+      default:
+        System.out.println("Opcao invalida. Por favor, escolha 1 para deposito ou 2 para saque.");
+        break;
+    }
   }
 }
